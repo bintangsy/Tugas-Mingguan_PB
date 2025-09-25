715fa3b1b5058518165f27ff4eed755a7ff04ce0
bintangsy <masbinsya@gmail.com>
2025-09-25 23:36:31 +0700

    commit pertama

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Profil Saya</h1>
+    <p>Nama: Bintang</p>
+    <p>NIM: 123456789</p>
+    <p>Jurusan: Teknik Informatika</p>
+    
+</body>
+</html>

a339793a66c91fb624fc491552c59e7f2d171fcd
bintangsy <masbinsya@gmail.com>
2025-11-12 22:27:17 +0700

    commit terbaru

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,5 @@ Route::get('/berita/{slug}', [BeritaController::class, 'datatampil']);
 
 Route::get('/mahasiswa', [MahasiswaController::class, 'index'])->name('mahasiswa');
 
-Route::get('/tambahmahasiswa', [MahasiswaController::class, 'tambahmahasiswa'])->name('tambahmahasiswa');
 
-Route::post('/insertdata', [MahasiswaController::class, 'insertdata'])->name('insertdata');
 
